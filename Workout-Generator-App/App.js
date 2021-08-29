a9441a0f41da4e352e4b02158c92d58d34977e2b
Daniel Safarov <safarovd@oregonstate.edu>
2021-08-29 00:07:05 -0700

    feature: added blinking text component for memes

--- Workout-Generator-App/App.js
+++ Workout-Generator-App/App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Platform, StyleSheet, View, Text, } from 'react-native';
 
-export default function App() {
-  return (
+class BlinkingText extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+ 
+    // Change the state every second 
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 
+    // Define any blinking time.
+    300);
+  }
+ 
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text style = {{ color: '#9C27B0', fontWeight: 'bold', fontSize : 20 , marginTop : 10 }}>{display}</Text>
+    );
+  }
+}
+
+export default class App extends Component {
+  render(){
+    return (
     <View style={styles.container}>
-      <Text>If Minh actually quit wasting time, maybe we would be done by now...</Text>
+      <BlinkingText text = 'If Minh actually quit wasting time,'/>
+      <BlinkingText text = ' maybe we would be done by now...🥴'/>
       <StatusBar style="auto" />
     </View>
   );
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
 });
