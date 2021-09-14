a667d392f68a900c87e426f76d5af85b510ece32
Minh Nguyen <minhng2598@gmail.com>
2021-09-13 22:15:21 -0700

    Added react-native-paper and react-native-vector-icons for android and ios

--- WorkoutGeneratorApp/screens/WorkoutGeneratorScreen/WorkoutGeneratorScreen.tsx
+++ WorkoutGeneratorApp/screens/WorkoutGeneratorScreen/WorkoutGeneratorScreen.tsx
@@ -4,9 +4,16 @@ import {
   View,
 } from 'react-native';
 
+import { Appbar } from 'react-native-paper';
+
+
 function WorkoutGeneratorScreen() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {/* TODO: Make sure you can see this icon */}
+        <Appbar>
+          <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
+        </Appbar>
         <Text>Generator Screen</Text>
       </View>
     );
