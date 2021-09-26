1c94575e76dbeb31a545217771dca75265d2f321
Daniel Safarov <safarovd@oregonstate.edu>
2021-09-26 13:40:33 -0700

    only importing what is needed

--- WorkoutGeneratorApp/App.tsx
+++ WorkoutGeneratorApp/App.tsx
@@ -7,8 +7,7 @@
  *
  * @format
  */
-// import { NavigationContainer } from '@react-navigation/native';
-import * as React from 'react';
+import React, { useState } from 'react'
 import { BottomNavigation } from 'react-native-paper';
 
 import WorkoutGeneratorScreen from './screens/WorkoutGeneratorScreen/WorkoutGeneratorScreen';
@@ -16,11 +15,11 @@ import SavedWorkoutsScreen from './screens/SaveScreens/SavedWorkoutsScreen';
 import SavedFiltersScreen from './screens/SaveScreens/SavedFiltersScreen';
 
 const App = () => {
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: 'workoutgenerator', title: 'Workout Generator', icon: 'home' },
-    { key: 'savedworkouts', title: 'Saved Workouts', icon: 'floppy' },
-    { key: 'savedfilters', title: 'Saved Filters', icon: 'filter' },
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'workoutgenerator', title: 'Workout Generator', icon: 'home'},
+    { key: 'savedworkouts', title: 'Saved Workouts', icon: 'floppy'},
+    { key: 'savedfilters', title: 'Saved Filters', icon: 'filter'},
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
